a78bc9c067b755179b7918674943ecdd0fb06a2a
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2023-11-27 18:08:32 +0100

    Start implementing home screen (event, state, screen, viewmodel)

--- shared/src/commonMain/kotlin/com/vml/tutorial/plantshop/App.kt
+++ shared/src/commonMain/kotlin/com/vml/tutorial/plantshop/App.kt
@@ -3,21 +3,33 @@ package com.vml.tutorial.plantshop
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.vml.tutorial.plantshop.core.presentation.PlantShopTheme
 import com.vml.tutorial.plantshop.di.AppModule
 import com.vml.tutorial.plantshop.plants.presentation.home.components.HomeScreen
+import com.vml.tutorial.plantshop.plants.presentation.home.components.HomeScreenViewModel
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 
 @Composable
 fun App(
     appModule: AppModule
 ) {
     PlantShopTheme {
+        val homeScreenViewModel = getViewModel(
+            key = "home-screen",
+            factory = viewModelFactory {
+                HomeScreenViewModel(appModule.plantsDataSource.getPlants())
+            }
+        )
+        val homeScreenState by homeScreenViewModel.state.collectAsState()
+
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
-            // Fetch plants -> appModule.plantsDataSource.getPlants()
-            HomeScreen()
+            HomeScreen(homeScreenState, homeScreenViewModel::onEvent)
         }
     }
 }
