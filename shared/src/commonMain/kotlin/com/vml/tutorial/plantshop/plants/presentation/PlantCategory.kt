62d334ef92a1f8826870feb49d3a6927d73bb27e
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2023-12-17 02:26:12 +0100

    Implement category selector

---
+++ shared/src/commonMain/kotlin/com/vml/tutorial/plantshop/plants/presentation/PlantCategory.kt
@@ -0,0 +1,8 @@
+package com.vml.tutorial.plantshop.plants.presentation
+
+enum class PlantCategory(val type: String) {
+    GREEN("green_plants"),
+    FLOWER("flowers"),
+    INDOOR("indoor_plants"),
+    NONE("")
+}
