48b29bb6693de1be0704147843e68836eae63e5e
gergo.koleszar <kole.geri@gmail.com>
2024-01-18 10:31:33 +0100

    Fixed iOS String/NSString issue, Added missing GoogleService-Info.plist to iOS part

--- shared/src/iosMain/kotlin/com/vml/tutorial/plantshop/core/utils/exts/StringExt.kt
+++ shared/src/iosMain/kotlin/com/vml/tutorial/plantshop/core/utils/exts/StringExt.kt
@@ -1,9 +1,13 @@
 package com.vml.tutorial.plantshop.core.utils.exts
 
+import kotlinx.cinterop.BetaInteropApi
 import platform.Foundation.NSPredicate
+import platform.Foundation.NSString
+import platform.Foundation.create
 
+@OptIn(BetaInteropApi::class)
 actual fun String.isValidEmail(): Boolean {
-    val emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    val emailRegEx: NSString = NSString.create(string = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
     val emailPred = NSPredicate.predicateWithFormat("SELF MATCHES %@", emailRegEx)
 
     return emailPred.evaluateWithObject(this)
