c5ff7778d6f64f2d6df2cb9ec9e88809b6fc2457
gergo.koleszar <kole.geri@gmail.com>
2024-01-29 14:59:31 +0100

    Fix some previously fixed issues

--- shared/src/iosMain/kotlin/com/vml/tutorial/plantshop/core/utils/exts/StringExt.kt
+++ shared/src/iosMain/kotlin/com/vml/tutorial/plantshop/core/utils/exts/StringExt.kt
@@ -1,9 +1,11 @@
 package com.vml.tutorial.plantshop.core.utils.exts
 
 import platform.Foundation.NSPredicate
+import platform.Foundation.NSString
+import platform.Foundation.create
 
 actual fun String.isValidEmail(): Boolean {
-    val emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    val emailRegEx = NSString.create(string = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
     val emailPred = NSPredicate.predicateWithFormat("SELF MATCHES %@", emailRegEx)
 
     return emailPred.evaluateWithObject(this)
